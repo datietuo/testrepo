00c78f06c3e41d7cf3b6a656ec7254821c092eee
datietuo <152195603+datietuo@users.noreply.github.com>
2024-01-09 17:57:11 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+employee_list = [
+   {"id": 12345, "name": "John", "department": "Kitchen"},
+   {"id": 12456, "name": "Paul", "department": "House Floor"},
+   {"id": 12478, "name": "Sarah", "department": "Management"},
+   {"id": 12434, "name": "Lisa", "department": "Cold Storage"},
+   {"id": 12483, "name": "Ryan", "department": "Inventory Mgmt"},
+   {"id": 12419, "name": "Gill", "department": "Cashier"}
+]
+
+d = {e["id"]:e["name"][0] for e in employee_list}
+
+print(d)
