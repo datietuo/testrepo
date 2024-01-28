d8985f07d34f50bd462512dab0e1f52bb0535fad
datietuo <datietuo@gmail.com>
2024-01-28 11:26:14 -0500

    message

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,8 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+// const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+
 
 var breakfastMenuHTML = breakfastMenu.map((item,index)=>`<p>Item ${index+1}:${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML=breakfastMenuHTML;
@@ -16,3 +18,4 @@ for(var i = 0; i<dessertMenu.length; i++){
     dessertMenuItem += `<p>Item ${i+1}:${dessertMenu[i]}</p>`;
 }
 document.getElementById('dessertMenuItems').innerHTML = dessertMenuItem;
+
